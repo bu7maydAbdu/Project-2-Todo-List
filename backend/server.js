a23746656f57e750ca6b6496da6f68aa5776d380
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2021-12-19 21:02:47 +0300

    created get and post function

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,38 @@ const app=express()
 const db=require('./db')
 const Todo=require('./todo')
 
+app.use(express.json())
+
 
 app.get('/', (req, res)=>{
 res.json('GET / is working')
 })
+
+
+app.get('/tasks', (req, res)=>{
+    Todo.find({}, (err, data)=>{
+        if(err){
+            console.log('ERROR: ', err)
+        }else{
+            res.json(data)
+        }
+    })
+    
+    })
+    
+
+    app.post('/tasks', (req, res)=>{
+        
+        Todo.create(req.body, (err, newData)=>{
+            if(err){
+                console.log('ERROR: ', err)
+            }else{
+                res.status(201).json(newData)
+            }
+        })
+        
+        })
+        
 
 
 app.listen(5000, ()=>{
