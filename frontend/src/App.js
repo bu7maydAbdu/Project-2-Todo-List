5f219ad0e57f9cc7b87e5c04d58a29651bcdbd73
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2021-12-23 00:24:28 +0300

    added few functions
    added( create new task, delete, task , and some decoration  on completed tasks "line-through")
    
    !!! check boxes  still not  completed rn

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 import axios from "axios";
 import Todo from "./components/Todo";
+import Add from "./components/Add";
 
 export default function App() {
   const [tasks, setTasks] = useState([]);
@@ -27,8 +28,45 @@ export default function App() {
       });
   };
 
+
+
+  const postNewTodo = (body) => {
+    // console.log("func postNewTodo from APP");
+    // {"title":"task 5","isCompleted": false}
+    axios
+    .post("http://localhost:5000/tasks",body)
+    .then((response) => {
+      // console.log('RESPONSE: ', response);
+      console.log("DATA: ", response.data);
+      // setTasks(response.data);
+      getData()
+      // change react hooks state using spread operator
+    })
+    .catch((err) => {
+      console.log("ERR: ", err);
+    });
+  };
+
+  const dltTask=(id)=>{
+    axios
+    .delete(`http://localhost:5000/tasks/${id}`)
+    .then((response) => {
+      // console.log('RESPONSE: ', response);
+      console.log("DATA: ", response.data);
+      // setTasks(response.data);
+      getData()
+      // change react hooks state using spread operator
+    })
+    .catch((err) => {
+      console.log("ERR: ", err);
+    });
+  }
+
+
+
+
   const mapOverTasks = tasks.map((taskObj, i) => (
-    <Todo key={i} task={taskObj} />
+    <Todo key={i} task={taskObj} dltTsk={dltTask}/>
   ));
 
   return (
@@ -37,7 +75,10 @@ export default function App() {
       {/* when click on this button 
       should call function bring Data */}
       <button onClick={getData}>GET TASKS</button>
+      <Add createFunc={postNewTodo} />
       {mapOverTasks}
+
+      
     </div>
   );
 }
--- frontend/src/components/Todo.js
+++ frontend/src/components/Todo.js
@@ -2,11 +2,20 @@ import React from 'react'
 
 export default function Todo(props) {
 
+
+
+  
+
   const {_id,
     title, isCompleted}=props.task
   return (
     <div className='Todo'>
        
-      <p>TITLE: {title}</p>
+     
+      <input type="checkbox" checked={isCompleted} />
+      <span style={{ textDecoration:isCompleted?'line-through':"none" }}>{title}</span>
+      <button className='delete-button' onClick={()=>{
+        props.dltTsk(_id)
+      }}>X</button>
     </div>
   )}
